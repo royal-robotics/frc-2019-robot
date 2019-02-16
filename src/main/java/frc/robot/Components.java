cd654fb97e9cf84650a9bb2e3abba82048dc8ad3
Connor Blomquist <connor.blomquist@outlook.com>
2019-02-16 12:33:58 -0800

    Assign motor controllers CAN ids

--- src/main/java/frc/robot/Components.java
+++ src/main/java/frc/robot/Components.java
@@ -10,12 +10,12 @@ import edu.wpi.first.wpilibj.Victor;
 
 public final class Components {
     public static class DriveBase {
-        public static TalonSRX leftDrive1 = new TalonSRX(0);
-        public static Victor leftDrive2 = new Victor(1);
-        public static Victor leftDrive3 = new Victor(2);
-        public static Talon rightDrive1 = new Talon(3);
-        public static Victor rightDrive2 = new Victor(4);
-        public static Victor rightDrive3 = new Victor(5);
+        public static TalonSRX leftDrive1 = new TalonSRX(7);
+        public static Victor leftDrive2 = new Victor(5);
+        public static Victor leftDrive3 = new Victor(3);
+        public static Talon rightDrive1 = new Talon(8);
+        public static Victor rightDrive2 = new Victor(6);
+        public static Victor rightDrive3 = new Victor(4);
         public static DoubleSolenoid climb = new DoubleSolenoid(0, 0, 7);
         public static Encoder driveLeft = new Encoder(10, 11, true);
         public static Encoder driveRight = new Encoder(12, 13, false);
@@ -23,9 +23,9 @@ public final class Components {
 
     public static class Elevator
     {
-        public static Talon elevator1 = new Talon(6);
-        public static Talon elevator2 = new Talon(7);
-        public static Talon carriage = new Talon(8);
+        public static Talon elevator1 = new Talon(9);
+        public static Talon elevator2 = new Talon(10);
+        public static Talon carriage = new Talon(11);
         public static Solenoid lift = new Solenoid(0, 10);
         public static DoubleSolenoid carriageShift = new DoubleSolenoid(1, 0, 7);
         public static DoubleSolenoid carriageRock = new DoubleSolenoid(1, 0, 7);
@@ -35,14 +35,13 @@ public final class Components {
 
 
     public static class Hatch {
-        public static Talon hatchArm = new Talon(9);
-        public static Victor hatchRoller = new Victor(10);
+        public static Talon hatchArm = new Talon(1);
+        public static Victor hatchRoller = new Victor(2);
     }
 
     public static class BallIntake {
-        public static Talon ballIntake1 = new Talon(11);
-        public static Talon ballIntake2 = new Talon(12);
-        public static Talon ballIntake3 = new Talon(13);
+        public static Talon ballIntake1 = new Talon(12);
+        public static Talon ballIntake2 = new Talon(13);
         public static DoubleSolenoid ballIntake4 = new DoubleSolenoid(0, 0, 7);
     }
 }
