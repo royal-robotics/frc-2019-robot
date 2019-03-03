9ec23e9556a8ea67e0ff56fde2bed9a2644e0f33
Connor Blomquist <connor.blomquist@outlook.com>
2019-03-03 14:06:13 -0800

    Tune ElevatorPositionHolder PID values

--- src/main/java/frc/robot/subsystems/elevator/ElevatorPositionHolder.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorPositionHolder.java
@@ -4,20 +4,18 @@ import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.*;
 
 public class ElevatorPositionHolder extends PIDController {
-    // Output/Input Units: power per inch
-    private static final double Kp = 0.25;
-    private static final double Ki = 0.0;
-    private static final double Kd = 0.0;
-
-    private static final double absoluteMaxPower = 0.4;
-
     private static final double LoopIntervalMs = 50.0;
     private static final double LoopInterval = LoopIntervalMs / 1000.0;
+
+    // Output/Input Units: power per inch
+    private static final double Kp = 0.22;
+    private static final double Ki = 0.04 / (1000.0 / LoopIntervalMs);
+    private static final double Kd = 0.005;
 
     public ElevatorPositionHolder(PIDSource source, PIDOutput output) {
         super(Kp, Ki, Kd, source, output, LoopInterval);
 
-        this.setOutputRange(-absoluteMaxPower, absoluteMaxPower);
+        this.setOutputRange(-0.4, 0.5);
         //this.setPercentTolerance(0.05);
     }
 
