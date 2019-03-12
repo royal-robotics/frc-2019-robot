d2cec8fb7437dccfa1d020fe79ae0da30cf90315
Brian Blomquist <brianblo@microsoft.com>
2019-03-12 16:01:31 -0700

    Add addition auto routines and mirrors.

--- src/main/java/frc/robot/autonomous/routines/CenterLeftFrontHatchAutoRoutine.java
+++ src/main/java/frc/robot/autonomous/routines/CenterLeftFrontHatchAutoRoutine.java
@@ -10,13 +10,13 @@ import frc.robot.subsystems.drivebase.DriveController;
 import frc.robot.subsystems.elevator.ElevatorController;
 import frc.robot.subsystems.hatch.HatchController;
 
-public class RightRocketAutoRoutine extends AutoRoutine {
+public class CenterLeftFrontHatchAutoRoutine extends AutoRoutine {
     private final Marker _parent;
     private final DriveController _driveController;
     private final ElevatorController _elevatorController;
     private final HatchController _hatchController;
 
-    public RightRocketAutoRoutine(Robot robot) {
+    public CenterLeftFrontHatchAutoRoutine(Robot robot) {
         super();
 
         _parent = this.logger.marker;
@@ -28,14 +28,10 @@ public class RightRocketAutoRoutine extends AutoRoutine {
     @Override
     protected List<AutoStep> createRoutine() {
         return Arrays.asList(
-            new TrajectoryDriveStep(_parent, _driveController, "rightRocket1", true),
-            new ElevatorMoveStep(_parent, _elevatorController, 60),
-            new DriveStraightStep(_parent, _driveController, 32.0, 40.0, 60),
-            new HatchShootStep(_parent, _hatchController),
+            new TrajectoryDriveStep(_parent, _driveController, "centerLeftFrontHatch", false),
             new ElevatorMoveStep(_parent, _elevatorController, 5),
-            new ElevatorMoveStep(_parent, _elevatorController, 0),
-            new TrajectoryDriveStep(_parent, _driveController, "rightRocket2", true),
-            new TrajectoryDriveStep(_parent, _driveController, "rightRocket3", false)
+            new HatchShootStep(_parent, _hatchController),
+            new ElevatorMoveStep(_parent, _elevatorController, 9)
         );
     }
 }
