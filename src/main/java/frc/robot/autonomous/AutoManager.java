f4bd235293b9e8c5ab7191e43492ede057ac6bcd
Programmer <fakeemail@fakeemail.com>
2019-03-12 22:23:01 -0700

    Added Auto Routine Selector and messages

--- src/main/java/frc/robot/autonomous/AutoManager.java
+++ src/main/java/frc/robot/autonomous/AutoManager.java
@@ -3,6 +3,7 @@ package frc.robot.autonomous;
 import frc.libs.autonomous.*;
 import frc.libs.autonomous.buildingblocks.*;
 import frc.robot.*;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 
 public class AutoManager {
     private final Robot robot;
@@ -21,7 +22,9 @@ public class AutoManager {
         stopAutonomous();
 
         currentRoutine = autoChooser.getSelectedRoutine();
-        currentRoutine.start();
+        if (currentRoutine != null) {
+            currentRoutine.start();
+        } 
     }
 
     public void stopAutonomous()
@@ -31,6 +34,72 @@ public class AutoManager {
             currentRoutine.stop();
             currentRoutine = null;
             LoggingContext.init();
+        }
+    }
+
+    public void printCurrentAutoRoutine() {
+        SmartDashboard.putNumber("getFieldStartPosition", Controls.getFieldStartPosition());
+        
+        switch(Controls.getFieldStartPosition()) {
+            case 3: {
+                switch(Controls.getAutoRoutineId()) {
+                    case 1: {
+                        SmartDashboard.putString("AutoRoutine-Name", "RightFrontHatchAutoRoutine");
+                        break;
+                    }
+
+                    case 2: {
+                        SmartDashboard.putString("AutoRoutine-Name", "RightRocketAutoRoutine");
+                        break;
+                    }
+
+                    case 3: {
+                        SmartDashboard.putString("AutoRoutine-Name", "RightCargoAutoRoutine");
+                        break;
+                    }
+
+                    default: {
+                        SmartDashboard.putString("AutoRoutine-Name", "No Routine");
+                        break;
+                    }
+                }
+                break;
+            }
+
+            case 2: {
+                SmartDashboard.putString("AutoRoutine-Name", "CenterRightFrontHatchAutoRoutine");
+                break;    
+            }
+
+            case 10: {
+                SmartDashboard.putString("AutoRoutine-Name", "CenterLeftFrontHatchAutoRoutine");
+                break;
+            }
+
+            case 9: {
+                switch(Controls.getAutoRoutineId()){
+                    case 1: {
+                        SmartDashboard.putString("AutoRoutine-Name", "LeftFrontHatchAutoRoutine");
+                        break;
+                    }
+
+                    case 2: {
+                        SmartDashboard.putString("AutoRoutine-Name", "LeftRocketAutoRoutine");
+                        break;
+                    }
+
+                    default: {
+                        SmartDashboard.putString("AutoRoutine-Name", "No Routine");
+                        break;
+                    }
+                }
+                break;
+            }
+
+            default: {
+                SmartDashboard.putString("AutoRoutine-Name", "No Routine");
+                break;
+            }
         }
     }
 }
